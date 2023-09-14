4008a971e4667e4b2842be5312c5fd886faac8f7
hyojining <gywls1390@gmail.com>
2023-09-14 11:36:06 +0900

    board #1 (reply, paging x)

--- mysite02/src/main/java/com/poscodx/mysite/web/mvc/board/WriteFormAction.java
+++ mysite02/src/main/java/com/poscodx/mysite/web/mvc/board/WriteFormAction.java
@@ -13,25 +13,7 @@ public class WriteFormAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-//		String g_noParam = request.getParameter("g_no");
-//		Long g_no = Long.parseLong(g_noParam);
-//		
-//		String o_noParam = request.getParameter("o_no");
-//		Long o_no = Long.parseLong(o_noParam);
-//		
-//		String depthParam = request.getParameter("depth");
-//		Long depth = Long.parseLong(depthParam);
-//		
-//		if (g_noParam != null && !g_noParam.isEmpty()) {
-//            request.setAttribute("g_no", g_no);
-//        }
-//		if (o_noParam != null && !o_noParam.isEmpty()) {
-//            request.setAttribute("o_no", o_no);
-//        }
-//		if (depthParam != null && !depthParam.isEmpty()) {
-//            request.setAttribute("depth", depth);
-//        }
+
 		WebUtil.forward("board/write", request, response);
 	}
 
