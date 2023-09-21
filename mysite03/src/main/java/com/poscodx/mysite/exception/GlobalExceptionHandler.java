552eb13b470a32d09c34689abb3faf005e8bc34f
hyojining <102501038+hyojining@users.noreply.github.com>
2023-09-21 12:17:14 +0900

    logging #3

--- mysite03/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.poscodx.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 // 여러 컨트롤러 클래스에서 발생하는 예외를 한 곳에서 처리
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	//전체 애플리케이션에서 발생하는 예외를 처리
 	@ExceptionHandler(Exception.class)
@@ -17,8 +20,9 @@ public class GlobalExceptionHandler {
 		// 1. 로깅(Logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		System.out.println(errors.toString());
-		
+//      System.out.println(errors.toString());
+		logger.error(errors.toString());
+
 		// 2. 사과 페이지
 		model.addAttribute("errors", errors.toString());
 		return "error/exception";
