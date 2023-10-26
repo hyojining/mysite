20271bc3ecda1c9244455ef684c12b5427270925
hyojining <102501038+hyojining@users.noreply.github.com>
2023-10-26 09:47:55 +0900

    json exception 처리

--- mysite06/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ mysite06/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.poscodx.mysite.exception;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.poscodx.mysite.dto.JsonResult;
 
 /**
  * 애플리케이션 내에서 발생하는 예외를 중앙에서 처리하고, 
@@ -23,11 +29,29 @@ public class GlobalExceptionHandler {
 	
 	//전체 애플리케이션에서 발생하는 예외를 처리
 	@ExceptionHandler(Exception.class)
-	public String handlerException(Model model, Exception e) {
+	public void handlerException(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception {
+		// 0. 요청 구분
+		// json 요청: accept에 application/json (o)
+		// html 요청: accept에 application/json (x)
+		String accept = request.getHeader("accept");
 		
 		// 1. 404 Error 처리
 		if(e instanceof NoHandlerFoundException) {
-			return "error/404";
+			if(accept.matches(".*application/json.*")) {
+				JsonResult jsonResult = JsonResult.fail("404 Not Found");
+				String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
+				
+				response.setStatus(HttpServletResponse.SC_OK);
+				response.setContentType("application.json; charset=utf-8");
+				
+				OutputStream os = response.getOutputStream();
+				os.write(jsonString.getBytes("utf-8"));
+			} else {
+				request
+					.getRequestDispatcher("/WEB-INF/views/error/404.jsp")
+					.forward(request, response);
+			}
+			return;
 		}
 		
 		// 2. 로깅(Logging)
@@ -36,7 +60,20 @@ public class GlobalExceptionHandler {
 		logger.error(errors.toString());
 
 		// 3. 사과 페이지
-		model.addAttribute("errors", errors.toString());
-		return "error/exception";
+		if(accept.matches(".*application/json.*")) {
+			JsonResult jsonResult = JsonResult.fail(errors.toString());
+			String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
+			
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType("application.json; charset=utf-8");
+			
+			OutputStream os = response.getOutputStream();
+			os.write(jsonString.getBytes("utf-8"));
+		} else {
+			request.setAttribute("errors", errors.toString());
+			request
+				.getRequestDispatcher("/WEB-INF/views/error/exception.jsp")
+				.forward(request, response);
+		}
 	}
 }
